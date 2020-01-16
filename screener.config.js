f3fb760f120c2fab21da840be0076f800907f1f6
dylanlacey <github@dylanlacey.com>
2020-01-16 11:21:50 +1000

    Add Screener support.
    Some config moved from .storybook/main.js to config.js, as Screener seems to look at the later exclusively.

---
+++ screener.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  projectRepo: 'dylanlacey/two_hard_problems',
+  storybookConfigDir: '.storybook',
+  storybookStaticDir: 'public',
+  apiKey: '1e3f165b-0d39-4800-9f10-8220d25a1690',
+  resolution: '1024x768'
+};
