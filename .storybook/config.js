012801422e273f52d3c9d1eec85a9463a29f1846
dylanlacey <github@dylanlacey.com>
2020-01-23 07:58:27 +1000

    Commit config just in case

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,16 @@
+// module.exports = {
+//   stories: ['../src/**/*.stories.js'],
+//   addons: [
+//     '@storybook/preset-create-react-app',
+//     '@storybook/addon-actions',
+//     '@storybook/addon-links',
+//   ],
+// };
+
+// if (typeof window === 'object') {
+//   window.__screener_storybook__ = require('@storybook/react').getStorybook;
+// }
+
 import { configure } from '@storybook/react';
 import '../src/index.css';
 
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
